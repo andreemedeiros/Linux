e91170cf47afe9e8350cd415e439cdd3c37077eb
André Escarião <andre.escariao1@gmail.com>
2023-05-14 19:19:18 -0300

    Delete Red_Hat_Enterprise_Linux/Guia_Linux directory

--- Red_Hat_Enterprise_Linux/Guia_Linux/Shell_Script/for.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Este é um script que demonstra os laços for
-lista_de_argumentos="1 2 4 7 8 4"
-for argumento in $lista_de_argumentos;
-do
-echo "o valor atual da variável argumento é $argumento"
-echo "o dobro deste valor é $(( argumento * 2 ))"
-done
--- Red_Hat_Enterprise_Linux/Guia_Linux/Shell_Script/while.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Este é um script que demonstra os laços while
-contador=0
-while (($contador <= 10));
-do
-echo "Contador = $contador"
-contador=$((contador + 1))
-done
