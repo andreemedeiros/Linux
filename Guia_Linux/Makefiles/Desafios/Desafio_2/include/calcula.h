f356e6450d59c774277c764ce31235cd20eedc2f
André Escarião <andre.escariao1@gmail.com>
2023-05-14 19:32:37 -0300

    Add files via upload

---
+++ Guia_Linux/Makefiles/Desafios/Desafio_2/include/calcula.h
@@ -0,0 +1,26 @@
+/* include/calcula.h.  Generated from calcula.h.in by configure.  */
+/* include/calcula.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "utilizando_autotools"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "andrekarine.medeiros@ee.ufcg.edu.br"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "utilizando_autotools"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "utilizando_autotools 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "utilizando_autotools"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
